ad3de6ee62968389632d8fa54abfd13aaf46e5cb
sebat <sebat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-16 21:27:36 +0000

    Add language support en_US
    git-svn-id: http://plugins.svn.wordpress.org/wp1/trunk@410826 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp1.php
+++ wp1.php
@@ -4,13 +4,10 @@ Plugin Name: WP Google+1
 Plugin URI: http://www.goopl.de
 Description: show all google +1 counts on the article overview
 Author: Sebastian Thiele
-Version: 0.1
+Version: 0.2
 Author URI: http://sebastian.thiele.me
 */
-<<<<<<< HEAD
 $wpg1Options = get_option('wpg1');
-=======
->>>>>>> 8ed133157d633890d5eb45af63e5813407b3d06b
 $plugindir = basename(dirname(__FILE__));
 load_plugin_textdomain( 'wpg1', 'wp-content/plugins/' . $plugindir.'/lang', false );
 include_once('wp1-menue.php');
@@ -41,11 +38,7 @@ function wpg1_article_colum_content($names)
     }
 }
 
-<<<<<<< HEAD
 function wpg1_head($content)
-=======
-function wpg1_admin_head($content)
->>>>>>> 8ed133157d633890d5eb45af63e5813407b3d06b
 {
     print '<script type="text/javascript" src="https://apis.google.com/js/plusone.js">
       {lang: \''.get_bloginfo('language').'\'}
@@ -59,12 +52,7 @@ function wpg1_adminmenue()
 
 add_filter('manage_posts_columns', 'wpg1_article_colum');
 add_filter('manage_posts_custom_column', 'wpg1_article_colum_content');
-<<<<<<< HEAD
 add_filter('admin_head', 'wpg1_head');
 add_action('admin_menu', 'wpg1_adminmenue');
 if($wpg1Options['wpg1-addtheme']) add_filter('wp_head', 'wpg1_head');
-=======
-add_filter('admin_head', 'wpg1_admin_head');
-add_action(	'admin_menu', 'wpg1_adminmenue');
->>>>>>> 8ed133157d633890d5eb45af63e5813407b3d06b
 ?>
